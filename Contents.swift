24c67726a49b8b682f1b15227447c96c13d68923
shankar2509 <leelashankargurram@gmail.com>
2024-07-15 09:56:04 +0530

    first commit

---
+++ Contents.swift
@@ -0,0 +1,79 @@
+import UIKit
+
+ 
+ // 1) Earth Atmosphere using if, if else and else
+ /*
+ let altitude = 50000 // Altitude in meters
+ 
+ if altitude < 12000 {
+ print("You are in the troposphere.")
+ } else if altitude < 50000 {  // between 12000- 50000
+ print("You are in the stratosphere.")
+ } else if altitude < 80000 {
+ print("You are in the mesosphere.")
+ } else if altitude < 1000000 {
+ print("You are in the thermosphere.")
+ } else {
+ print("You are in the exosphere.")
+ }
+ */
+ 
+
+//2) Number Sorting
+/*
+
+let number = 3
+
+switch number {
+
+case 0:
+    print("\(number) is zero")
+   // fallthrough
+default:
+    var num = "positive"
+    if number > 0{
+        num = "positive"
+    }else{
+        num = "Negative"
+    }
+    print("\(number) is \(num)")
+        
+}
+ 
+*/
+
+// 3rd
+
+
+/*
+for number in 1...100 {
+    let multiplied = number * 2
+    print(multiplied)
+}
+*/
+//4) while - 100  i need print 99
+
+
+ /*
+ var number = 100
+ 
+ while number == 100 { //execute the code until a condition changes.
+ number -= 1
+ print(" print ---> \(number)")
+ 
+ }
+ 
+ */
+
+//
+
+
+ /*
+let name = "Leela Shankar"
+
+for character in name {
+    print(character)
+}
+ */
+ 
+
